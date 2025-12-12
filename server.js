f3b6a121310faeb9a849f7fb2a71d8dce821b8f8
ballm <alisherzhanibek@mail.ru>
2025-12-12 11:56:38 +0500

    connecting server(test)

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+});
+app.listen(3000, () => console.log('Server running on http://localhost:3000'))
