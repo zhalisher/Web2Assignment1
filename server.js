3fbe4a0e49f459e545a8e271d08c7534fe66b656
csgoexampleyt-cyber <csgoexampleyt@gmail.com>
2025-12-14 14:09:38 +0500

    Modified server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-
-app.use(express.static('public'));
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
-app.listen(3000, () => console.log('Server running on http://localhost:3000'))
+app.listen(3000, () => console.log('Server running on http://localhost:3000'));
