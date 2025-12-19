112350723a67bb2f088fcd6b5ad7aee8ff55eb1a
ballm <alisherzhanibek@mail.ru>
2025-12-19 22:35:20 +0500

    minor changes (adding links and connection to each page + testing post and get )

--- server.js
+++ server.js
@@ -2,15 +2,26 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
-app.get('/index.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
+
 app.get('/contact.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'contact.html'));
+});
+
+app.post('/submit', (req, res) => {
+    console.log(req.body);
+
+    res.send(`
+        <h2>Thanks, ${req.body.name}!</h2>
+        <p>Email: ${req.body.email}</p>
+        <p>Message: ${req.body.message}</p>
+    `);
 });
 
 app.listen(3000, () => console.log('Server running on http://localhost:3000'));
