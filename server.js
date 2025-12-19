e63312774b342ae5eb6de3590702f4187852ef56
ballm <alisherzhanibek@mail.ru>
2025-12-19 23:11:09 +0500

    new pathes

--- server.js
+++ server.js
@@ -14,14 +14,17 @@ app.get('/contact.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'contact.html'));
 });
 
+app.get('/about.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'about.html'));
+});
+
+app.get('/index.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
 app.post('/submit', (req, res) => {
     console.log(req.body);
-
-    res.send(`
-        <h2>Thanks, ${req.body.name}!</h2>
-        <p>Email: ${req.body.email}</p>
-        <p>Message: ${req.body.message}</p>
-    `);
+    res.sendFile(path.join(__dirname, 'views', 'test.html'));
 });
 
 app.listen(3000, () => console.log('Server running on http://localhost:3000'));
