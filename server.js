bd8ef2b1d876cb39ff2f6471c3460f9bc4274488
petrichorrr006 <raven24112006@gmail.com>
2025-12-27 21:39:05 +0500

    add logging middleware to track HTTP requests

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const path = require('path');
 
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
